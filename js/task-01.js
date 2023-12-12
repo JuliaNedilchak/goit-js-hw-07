9d6edc5f67afae110d2219414d1f8096f7bc0d7e
JuliaNedilchak <julianedilchak@gmail.com>
2023-12-12 14:37:05 +0000

    final work

--- js/task-01.js
+++ js/task-01.js
@@ -1,15 +1,7 @@
-const itemLists = document.querySelector('#categories');
-let total = itemLists.children.length;
 const itemCategories = document.querySelectorAll('.item');
-const ulLists = document.querySelectorAll('ul');
+console.log(`Number of categories: ${itemCategories.length}`);
 
-const arrayUls = Array.from(ulLists).splice(1);
-const arrayItems = Array.from(itemCategories);
-
-arrayItems.forEach(arrayItem => {
-  console.log(`Category: ${arrayItem.firstElementChild.textContent}`);
+itemCategories.forEach(itemCategory => {
+  console.log(`Category: ${itemCategory.firstElementChild.textContent}`);
+  console.log(`Elements: ${itemCategory.lastElementChild.children.length}`);
 });
-arrayUls.forEach(arrayUl => {
-  console.log(`Elements: ${arrayUl.children.length}`);
-});
-console.log(`Number of categories: ${total}`);
